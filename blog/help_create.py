90c866287e9beac5e5015a23bc090a4a6e0d3327
andrey <zakurov@gmail.com>
2022-05-09 00:46:29 +0300

    podelal chutok))

---
+++ blog/help_create.py
@@ -0,0 +1,13 @@
+"""
+  Вспомогательные функции для облегчения разработки
+
+"""
+
+
+class Help_create:
+    def __init__(self):
+        self.flag = True
+
+    def line_string(self):
+        if self.flag:
+            print('+----------------------------')
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.utils import timezone
+from  .help_create import Help_create
 
 # Create your views here.
 
+br = Help_create()
+
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+    print(type(posts))
+    print('-----------')
+    print(posts.count)
+    print('-----------')
+    print(dir(posts))
+    br.line_string()
+    print(dir(render))
+    print('-----------')
+    br.line_string()
+    print(br.line_string())
+    context = {'posts':posts}
+#    context = {1: 123, 2:125}
+    return render(request, 'blog/post_list.html', context)
+
+def post_detail(request,pk):
+     post = get_object_or_404(Post, pk=pk)
+     return render(request, 'blog/post_detail.html', {'post': post})
